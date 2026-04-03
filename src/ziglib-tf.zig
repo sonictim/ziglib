5068e4afd644cae7b21f06faf85a9a62bb51b414
sonictim <soundguru@gmail.com>
2026-04-02 21:46:32 -0500

    some new aliases

--- src/ziglib-tf.zig
+++ src/ziglib-tf.zig
@@ -1,24 +1,25 @@
-const std = @import("std");
+pub const std = @import("std");
 pub const s = @import("string.zig");
 pub const str = s.str;
 pub const String = s.String;
 pub const json = @import("json.zig");
-pub const io = std.Io;
+pub const Io = std.Io;
 pub const a = std.mem.Allocator;
+pub const eql = std.mem.eql;
 
-pub fn print(comptime txt: []const u8) void {
+pub fn print(comptime txt: str) void {
     std.debug.print(txt ++ "\n", .{});
 }
-pub fn log(comptime txt: []const u8, args: anytype) void {
+pub fn log(comptime txt: str, args: anytype) void {
     std.log.err(txt ++ "\n", args);
 }
-pub fn err(comptime txt: []const u8, er: anyerror) void {
+pub fn err(comptime txt: str, er: anyerror) void {
     std.log.err(txt ++ " {} ({s})", .{ er, @errorName(er) });
 }
-pub fn warn(comptime txt: []const u8, args: anytype) void {
+pub fn warn(comptime txt: str, args: anytype) void {
     std.log.warn(txt ++ "\n", args);
 }
-pub fn debug(comptime txt: []const u8, args: anytype) void {
+pub fn debug(comptime txt: str, args: anytype) void {
     std.log.debug(txt ++ "\n", args);
 }
 
