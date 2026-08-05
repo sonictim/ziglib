0f06592c53abc502cb2ed05e7740a08b770755e5
sonictim <soundguru@gmail.com>
2026-08-05 00:04:33 -0500

    updates

--- src/zag.zig
+++ src/zag.zig
@@ -1,34 +1,37 @@
 pub const std = @import("std");
-pub const s = @import("string.zig");
-pub const str = s.str;
-pub const String = s.String;
+// pub const s = @import("string.zig");
+// pub const str = s.str;
+// pub const String = s.String;
 pub const json = @import("json.zig");
-pub const Io = std.Io;
+pub const io = std.Io;
 pub const a = std.mem.Allocator;
 pub const eql = std.mem.eql;
+// pub const text = @import("text.zig").Text;
+
+pub const str = []const u8;
+pub const cstr = [:0]const u8;
+pub const String = std.ArrayList(u8).empty;
 
 pub fn print(comptime txt: str) void {
     std.debug.print(txt ++ "\n", .{});
 }
 pub fn log(comptime txt: str, args: anytype) void {
-    std.log.err(txt ++ "\n", args);
+    std.log.err(txt, args);
 }
 pub fn err(comptime txt: str, er: anyerror) void {
     std.log.err(txt ++ " {} ({s})", .{ er, @errorName(er) });
 }
 pub fn warn(comptime txt: str, args: anytype) void {
-    std.log.warn(txt ++ "\n", args);
+    std.log.warn(txt, args);
 }
 pub fn debug(comptime txt: str, args: anytype) void {
-    std.log.debug(txt ++ "\n", args);
+    std.log.debug(txt, args);
 }
 
 pub fn list(comptime T: type) std.ArrayList(T) {
     return std.ArrayList(T).empty;
-    // don't forget to deinit with an allocator after returcning
 }
 
-pub fn map(comptime K: type, comptime V: type) std.AutoHashMap(K, V) {
+pub fn map(comptime K: type, comptime V: type) std.AutoHashMapUnmanaged(K, V) {
     return std.AutoHashMap(K, V).empty;
-    // don't forget to deinit with an allocator after returcning
 }
